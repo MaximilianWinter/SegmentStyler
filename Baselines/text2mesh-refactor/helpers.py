c59d5973f982113c8152e3330bb7aa2d3a4d5b91
Murilo Bellatini <murilobell@gmail.com>
2022-11-09 11:14:23 +0100

    Get img embeddings

--- Baselines/text2mesh-refactor/helpers.py
+++ Baselines/text2mesh-refactor/helpers.py
@@ -1,15 +1,18 @@
+from PIL import Image
 from neural_style_field import NeuralStyleField
 import clip
 import os
 import numpy as np
 import random
 from torchvision import transforms
+from utils import device
+
 
 def get_resolution(clipmodel=None):
     """
     Returns output resolution depending on model type 
     """
-    res = 224 
+    res = 224
     if clipmodel == "ViT-L/14@336px":
         res = 336
     if clipmodel == "RN50x4":
@@ -18,10 +21,11 @@ def get_resolution(clipmodel=None):
         res = 384
     if clipmodel == "RN50x64":
         res = 448
-    
+
     return res
 
-def get_background(background, torch=None, device=None):
+
+def get_background(background, torch=None):
     bg = None
     if background is not None:
         assert len(background) == 3
@@ -58,15 +62,15 @@ def check_previous_run(objbase, args):
                     shutil.rmtree(file_path)
             except Exception as e:
                 print('Failed to delete %s. Reason: %s' % (file_path, e))
-                
-                
+
 
 def get_clip(args, res):
     n_augs = args.n_augs
     dir = args.output_dir
     clip_model, preprocess = clip.load(args.clipmodel, device, jit=args.jit)
-    
-    clip_normalizer = transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
+
+    clip_normalizer = transforms.Normalize(
+        (0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
     # CLIP Transform
     clip_transform = transforms.Compose([
         transforms.Resize((res, res)),
@@ -74,14 +78,54 @@ def get_clip(args, res):
     ])
     return clip_model, preprocess, clip_transform
 
+
 def get_neural_style_network(args):
     # MLP Settings
     input_dim = 6 if args.input_normals else 3
     if args.only_z:
         input_dim = 1
     mlp = NeuralStyleField(args.sigma, args.depth, args.width, 'gaussian', args.colordepth, args.normdepth,
-                                args.normratio, args.clamp, args.normclamp, niter=args.n_iter,
-                                progressive_encoding=args.pe, input_dim=input_dim, exclude=args.exclude).to(device)
+                           args.normratio, args.clamp, args.normclamp, niter=args.n_iter,
+                           progressive_encoding=args.pe, input_dim=input_dim, exclude=args.exclude).to(device)
     mlp.reset_weights()
-    
+
     return mlp
+
+
+def get_text_embeddings(args, clip_model):
+    # Constrain all sources of randomness
+    if not args.no_prompt:
+        if args.prompt:
+            prompt = ' '.join(args.prompt)
+            prompt_token = clip.tokenize([prompt]).to(device)
+            encoded_text = clip_model.encode_text(prompt_token)
+
+            # Save prompt
+            with open(os.path.join(dir, prompt), "w") as f:
+                f.write("")
+
+            # Same with normprompt
+            norm_encoded = encoded_text
+
+    if args.normprompt is not None:
+        prompt = ' '.join(args.normprompt)
+        prompt_token = clip.tokenize([prompt]).to(device)
+        norm_encoded = clip_model.encode_text(prompt_token)
+
+        # Save prompt
+        with open(os.path.join(dir, f"NORM {prompt}"), "w") as f:
+            f.write("")
+
+        return encoded_text, norm_encoded
+
+
+def get_img_embeddings(self):
+
+    if self.args.image:
+        img = Image.open(self.args.image)
+        img = self.preprocess(img).to(device)
+        encoded_image = self.clip_model.encode_image(img.unsqueeze(0))
+        if self.args.no_prompt:
+            self.norm_encoded = encoded_image
+
+    return encoded_image
--- Baselines/text2mesh-refactor/model.py
+++ Baselines/text2mesh-refactor/model.py
@@ -4,29 +4,35 @@ from torch import optim, nn, utils, Tensor
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
 import pytorch_lightning as pl
-from helpers import get_resolution, get_neural_style_network
-from utils import device 
+from helpers import get_resolution, get_neural_style_network, get_text_embeddings, get_img_embeddings
+from utils import device
 from data_module import get_augumentations, get_mesh_with_attrb, get_clip
 import torch
 
 # define the LightningModule
+
+
 class Text2Mesh(pl.LightningModule):
     def __init__(self, args):
         super().__init__()
-        
+
         import config
         self.normweight = 1.0
         self.args = args
-        
+
         self.res = get_resolution(self.args.clipmodel)
         self.renderer = Renderer(dim=(self.res, self.res))
         self.mesh, attr = get_mesh_with_attrb(self.args)
         self.prior_color, self.background, self.objpath = attr
-        self.clip_model, self.preprocess, self.clip_transform = get_clip(self.args, self.res)
+        self.clip_model, self.preprocess, self.clip_transform = get_clip(
+            self.args, self.res)
         self.augment_transform, self.normaugment_transform, \
-        self.displaugment_transform = get_augumentations(self.args, self.res, self.clip_normalizer)
+            self.displaugment_transform = get_augumentations(
+                self.args, self.res, self.clip_normalizer)
         self.mlp = get_neural_style_network(self.args)
-        
+
+        encoded_text, norm_encoded = get_text_embeddings(self.clip_model)
+        encoded_image = get_img_embeddings()
 
     def training_step(self, batch, batch_idx):
         # training_step defines the train loop.
@@ -42,32 +48,34 @@ class Text2Mesh(pl.LightningModule):
         pass
 
     def configure_optimizers(self):
-        optim = torch.optim.Adam(self.mlp.parameters(), self.args.learning_rate, weight_decay=self.args.decay)
+        optim = torch.optim.Adam(self.mlp.parameters(
+        ), self.args.learning_rate, weight_decay=self.args.decay)
         activate_scheduler = self.args.lr_decay < 1 and self.args.decay_step > 0 and not self.args.lr_plateau
-        
+
         lr_scheduler = None
         if activate_scheduler:
-            lr_scheduler = torch.optim.lr_scheduler.StepLR(optim, step_size=self.args.decay_step, gamma=self.args.lr_decay)
-        
+            lr_scheduler = torch.optim.lr_scheduler.StepLR(
+                optim, step_size=self.args.decay_step, gamma=self.args.lr_decay)
+
         return [optim], [lr_scheduler]
-    
+
     def forward(self, x):
         pass
-    
+
     def validation_setp(self, batch, batch_idx):
         pass
-    
+
     def prepare_data(self):
         pass
-    
+
     def setup(self):
         pass
-    
+
     def train_dataloader(self):
         pass
-    
+
     def val_dataloader(self):
         pass
-    
+
     def test_dataloader(self):
         pass
