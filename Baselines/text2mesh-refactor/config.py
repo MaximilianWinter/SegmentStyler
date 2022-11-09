a0d0eaf168803e396ea300f6065b54fad9681284
Murilo Bellatini <murilobell@gmail.com>
2022-11-09 11:35:32 +0100

    Init class refactor

--- Baselines/text2mesh-refactor/config.py
+++ Baselines/text2mesh-refactor/config.py
@@ -4,10 +4,4 @@ import numpy as np
 
 args = globals()['args']
 
-torch.manual_seed(args.seed)
-torch.cuda.manual_seed(args.seed)
-torch.cuda.manual_seed_all(args.seed)
-random.seed(args.seed)
-np.random.seed(args.seed)
-torch.backends.cudnn.benchmark = False
-torch.backends.cudnn.deterministic = True
+
--- Baselines/text2mesh-refactor/model.py
+++ Baselines/text2mesh-refactor/model.py
@@ -1,38 +1,40 @@
+from neural_style_field import NeuralStyleField
+import clip
+from mesh import Mesh
+from Normalization import MeshNormalizer
 import os
 from render import Renderer
 from torch import optim, nn, utils, Tensor
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
 import pytorch_lightning as pl
-from helpers import get_resolution, get_neural_style_network, get_text_embeddings, get_img_embeddings
 from utils import device
-from data_module import get_augumentations, get_mesh_with_attrb, get_clip
 import torch
+import numpy as np
+import random
+from pathlib import Path
+from PIL import Image
 
-# define the LightningModule
 
-
-class Text2Mesh(pl.LightningModule):
+class Text2Mesh():
     def __init__(self, args):
         super().__init__()
 
-        import config
+        self.set_seed()
         self.normweight = 1.0
         self.args = args
 
-        self.res = get_resolution(self.args.clipmodel)
+        self.res = self.get_render_resolution()
         self.renderer = Renderer(dim=(self.res, self.res))
-        self.mesh, attr = get_mesh_with_attrb(self.args)
-        self.prior_color, self.background, self.objpath = attr
-        self.clip_model, self.preprocess, self.clip_transform = get_clip(
-            self.args, self.res)
+        self.mesh = self.get_mesh()
+        self.store_mesh_params()
+        self.clip_model, self.preprocess, self.clip_transform = self.get_clip()
         self.augment_transform, self.normaugment_transform, \
-            self.displaugment_transform = get_augumentations(
-                self.args, self.res, self.clip_normalizer)
-        self.mlp = get_neural_style_network(self.args)
+            self.displaugment_transform = self.get_augumentations()
+        self.mlp = self.get_neural_style_network()
 
-        encoded_text, norm_encoded = get_text_embeddings(self.clip_model)
-        encoded_image = get_img_embeddings()
+        self.encoded_text, self.norm_encoded = self.get_text_embeddings()
+        self.encoded_image = self.get_img_embeddings()
 
     def training_step(self, batch, batch_idx):
         # training_step defines the train loop.
@@ -79,3 +81,188 @@ class Text2Mesh(pl.LightningModule):
 
     def test_dataloader(self):
         pass
+
+    def set_seed(self):
+        torch.manual_seed(self.args.seed)
+        torch.cuda.manual_seed(self.args.seed)
+        torch.cuda.manual_seed_all(self.args.seed)
+        random.seed(self.args.seed)
+        np.random.seed(self.args.seed)
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+
+    def get_render_resolution(self):
+        """
+        Sets output resolution depending on model type
+        """
+        self.res = 224
+        if self.clipmodel == "ViT-L/14@336px":
+            self.res = 336
+        if self.clipmodel == "RN50x4":
+            self.res = 288
+        if self.clipmodel == "RN50x16":
+            self.res = 384
+        if self.clipmodel == "RN50x64":
+            self.res = 448
+
+        return self.res
+
+    def get_background(background, torch=None):
+        bg = None
+        if background is not None:
+            assert len(background) == 3
+            bg = torch.tensor(background).to(device)
+        return bg
+
+    def init_torch(torch, seed):
+        # Constrain all sources of randomness
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+        return torch
+
+    def check_previous_run(self):
+        if (not self.args.overwrite) and os.path.exists(os.path.join(self.args.output_dir, "loss.png")) and \
+                os.path.exists(os.path.join(self.args.output_dir, f"{self.objbase}_final.obj")):
+            print(f"Already done with {self.args.output_dir}")
+            exit()
+        elif self.args.overwrite and os.path.exists(os.path.join(self.args.output_dir, "loss.png")) and \
+                os.path.exists(os.path.join(self.args.output_dir, f"{self.objbase}_final.obj")):
+            import shutil
+            for filename in os.listdir(self.args.output_dir):
+                file_path = os.path.join(self.args.output_dir, filename)
+                try:
+                    if os.path.isfile(file_path) or os.path.islink(file_path):
+                        os.unlink(file_path)
+                    elif os.path.isdir(file_path):
+                        shutil.rmtree(file_path)
+                except Exception as e:
+                    print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+    def get_clip(self):
+        self.n_augs = self.args.n_augs
+        self.dir = self.args.output_dir
+        self.clip_model, self.preprocess = clip.load(
+            self.args.clipmodel, device, jit=self.args.jit)
+
+        self.clip_normalizer = self.transforms.Normalize(
+            (0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
+        # CLIP Transform
+        self.clip_transform = self.transforms.Compose([
+            self.transforms.Resize((self.res, self.res)),
+            self.clip_normalizer
+        ])
+        return self.clip_model, self.preprocess, self.clip_transform
+
+    def get_neural_style_network(self):
+        # MLP Settings
+        self.input_dim = 6 if self.args.input_normals else 3
+        if self.args.only_z:
+            self.input_dim = 1
+        self.mlp = NeuralStyleField(self.args.sigma, self.args.depth, self.args.width, 'gaussian', self.args.colordepth, self.args.normdepth,
+                                    self.args.normratio, self.args.clamp, self.args.normclamp, niter=self.args.n_iter,
+                                    progressive_encoding=self.args.pe, input_dim=self.input_dim, exclude=self.args.exclude).to(device)
+        self.mlp.reset_weights()
+
+        return self.mlp
+
+    def get_text_embeddings(self):
+        # Constrain all sources of randomness
+        if not self.args.no_prompt:
+            if self.args.prompt:
+                prompt = ' '.join(self.args.prompt)
+                prompt_token = clip.tokenize([prompt]).to(device)
+                self.encoded_text = self.clip_model.encode_text(prompt_token)
+
+                # Save prompt
+                with open(os.path.join(dir, prompt), "w") as f:
+                    f.write("")
+
+                # Same with normprompt
+                self.norm_encoded = self.encoded_text
+
+        if self.args.normprompt is not None:
+            prompt = ' '.join(self.args.normprompt)
+            prompt_token = clip.tokenize([prompt]).to(device)
+            self.norm_encoded = self.clip_model.encode_text(prompt_token)
+
+            # Save prompt
+            with open(os.path.join(dir, f"NORM {prompt}"), "w") as f:
+                f.write("")
+
+            return self.encoded_text, self.norm_encoded
+
+    def get_img_embeddings(self):
+
+        if self.args.image:
+            img = Image.open(self.args.image)
+            img = self.preprocess(img).to(device)
+            self.encoded_image = self.clip_model.encode_image(img.unsqueeze(0))
+            if self.args.no_prompt:
+                self.norm_encoded = self.encoded_image
+
+        return self.encoded_image
+
+    def get_augumentations(self):
+        # Augmentation settings
+        self.augment_transform = self.transforms.Compose([
+            self.transforms.RandomResizedCrop(self.res, scale=(1, 1)),
+            self.transforms.RandomPerspective(
+                fill=1, p=0.8, distortion_scale=0.5),
+            self.clip_normalizer
+        ])
+
+        # Augmentations for normal network
+        curcrop = self.args.normmincrop if self.args.cropforward else self.args.normmaxcrop
+
+        self.normaugment_transform = self.transforms.Compose([
+            self.transforms.RandomResizedCrop(
+                self.res, scale=(curcrop, curcrop)),
+            self.transforms.RandomPerspective(
+                fill=1, p=0.8, distortion_scale=0.5)
+        ])
+        cropiter = 0
+        cropupdate = 0
+        if self.args.normmincrop < self.args.normmaxcrop and self.args.cropsteps > 0:
+            cropiter = round(self.args.n_iter / (self.args.cropsteps + 1))
+            cropupdate = (self.args.maxcrop - self.args.mincrop) / cropiter
+
+            if not self.args.cropforward:
+                cropupdate *= -1
+
+        # Displacement-only augmentations
+        self.displaugment_transform = self.transforms.Compose([
+            self.transforms.RandomResizedCrop(self.res, scale=(
+                self.args.normmincrop, self.args.normmincrop)),
+            self.transforms.RandomPerspective(
+                fill=1, p=0.8, distortion_scale=0.5),
+            self.clip_normalizer
+        ])
+
+        return self.augment_transform, self.normaugment_transform, self.displaugment_transform
+
+    def make_output_dir(self):
+        self.objbase, _ = os.path.splitext(
+            os.path.basename(self.args.obj_path))
+        Path(self.args.output_dir).mkdir(parents=True, exist_ok=True)
+
+    def load_mesh(self):
+        self.mesh = Mesh(self.args.obj_path)
+        MeshNormalizer(self.mesh)()
+        return self.mesh
+
+    def store_mesh_params(self):
+        self.prior_color = torch.full(
+            size=(self.mesh.faces.shape[0], 3, 3), fill_value=0.5, device=device)
+        self.background = self.get_background(
+            self.args.background, torch, device)
+
+    def get_mesh(self):
+        self.make_output_dir()
+        self.check_previous_run()
+        self.mesh = self.load_mesh()
+        return self.mesh
