46238c3d487fa2bbce143580b2ddfdfb67951c69
Maximilian Winter <winter.maximilian@protonmail.com>
2022-11-19 22:05:29 +0100

    Bugfix in weight loading. (multi-prompt-experiments)

--- src/utils/processing.py
+++ src/utils/processing.py
@@ -51,7 +51,7 @@ def train(args, config, wand_proj='dl3d', team='meshers'):
     text2mesh_model = config["model"](args, base_mesh)
 
     if args.weights_path != "new":
-        text2mesh_model.mlp.load_state_dict(torch.load(args.model_path))
+        text2mesh_model.mlp.load_state_dict(torch.load(args.weights_path))
 
     # DEFINE OPTIMIZER
     optimizer = torch.optim.Adam(text2mesh_model.mlp.parameters(), args.learning_rate, weight_decay=args.decay)
