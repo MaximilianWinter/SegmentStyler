840450cf3f6310a27006bf72a4c8e737b99a3eea
Murilo Bellatini <murilobell@gmail.com>
2022-12-07 16:27:06 +0100

    Fix paths

--- src/helper/paths.py
+++ src/helper/paths.py
@@ -5,7 +5,7 @@ All relevant paths stored in constant variables
 import pathlib as pl
 
 # local paths
-root_path = pl.Path(__file__).parents[1]
+root_path = pl.Path(__file__).parents[2]
 LOCAL_RAW_DATA_PATH       = root_path / 'data/raw'
 LOCAL_INTERIM_DATA_PATH   = root_path / 'data/interim'
 LOCAL_PROCESSED_DATA_PATH = root_path / 'data/processed'
@@ -13,6 +13,9 @@ LOCAL_MODELS_PATH         = root_path / 'models'
 LOCAL_CREDENTIALS_PATH    = root_path / 'credentials'
 LOCAL_LOGGING_PATH        = root_path / 'logging/log.log'
 LOCAL_CONFIG_PATH         = root_path / 'src/config'
+
+NOTEBOOKS_PATH = root_path / 'notebooks'
+BASELINES_PATH = root_path / 'Baselines'
 
 # global paths
 GLOBAL_DATA_PATH          = pl.Path('/mnt/hdd')
