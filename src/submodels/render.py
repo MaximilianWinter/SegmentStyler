bdc2511d7039e2f1b05f96b275bb83617ff2d23b
Maximilian Winter <winter.maximilian@protonmail.com>
2022-12-12 03:05:38 +0100

    Add render_sampled_views_along_principal_axes. (multi-mlps)

--- src/submodels/render.py
+++ src/submodels/render.py
@@ -688,6 +688,132 @@ class Renderer:
         else:
             return images
 
+    def render_sampled_views_along_principal_axes(
+        self,
+        mesh,
+        singular_values, principal_axes, num_vertices,
+        num_views=8,
+        std=8,
+        center_elev=0,
+        center_azim=0,
+        center_point = torch.tensor([0.0, 0.0, 0.0]),
+        distance = 2.,
+        show=False,
+        lighting=True,
+        background=None,
+        mask=False,
+        return_views=False,
+    ):
+        # Front view with small perturbations in viewing angle
+        verts = mesh.vertices
+        faces = mesh.faces
+        n_faces = faces.shape[0]
+
+        # take principal axes with smalles singular_value
+        axis = principal_axes[:,singular_values.argmin()]
+        axis = axis/torch.linalg.norm(axis,ord=2)
+        #flip_idx = np.random.choice(2)
+        #axis = [axis, -axis][flip_idx]
+        #distance = singular_values.min().item()
+
+        center_elev = torch.asin(axis[1]).item()
+        center_azim = torch.atan2(axis[2], axis[0]).item()
+
+        elev = torch.cat(
+            (
+                torch.tensor([center_elev]),
+                torch.randn(num_views - 1) * np.pi / std + center_elev,
+            )
+        )
+        azim = torch.cat(
+            (
+                torch.tensor([center_azim]),
+                torch.randn(num_views - 1) * 2 * np.pi / std + center_azim,
+            )
+        )
+        images = []
+        masks = []
+        rgb_mask = []
+
+        if background is not None:
+            face_attributes = [
+                mesh.face_attributes,
+                torch.ones((1, n_faces, 3, 1), device=device),
+            ]
+        else:
+            face_attributes = mesh.face_attributes
+
+        for i in range(num_views):
+            camera_transform = get_camera_from_view_and_center(elev[i], azim[i], r=distance, center_point=center_point).to(device)
+            (
+                face_vertices_camera,
+                face_vertices_image,
+                face_normals,
+            ) = kal.render.mesh.prepare_vertices(
+                mesh.vertices.to(device),
+                mesh.faces.to(device),
+                self.camera_projection,
+                camera_transform=camera_transform,
+            )
+            image_features, soft_mask, face_idx = kal.render.mesh.dibr_rasterization(
+                self.dim[1],
+                self.dim[0],
+                face_vertices_camera[:, :, :, -1],
+                face_vertices_image,
+                face_attributes,
+                face_normals[:, :, -1],
+            )
+            masks.append(soft_mask)
+
+            if background is not None:
+                image_features, mask = image_features
+
+            image = torch.clamp(image_features, 0.0, 1.0)
+
+            if lighting:
+                image_normals = face_normals[:, face_idx].squeeze(0)
+                image_lighting = kal.render.mesh.spherical_harmonic_lighting(
+                    image_normals, self.lights
+                ).unsqueeze(0)
+                image = image * image_lighting.repeat(1, 3, 1, 1).permute(
+                    0, 2, 3, 1
+                ).to(device)
+                image = torch.clamp(image, 0.0, 1.0)
+
+            if background is not None:
+                background_mask = torch.zeros(image.shape).to(device)
+                mask = mask.squeeze(-1)
+                assert torch.all(
+                    image[torch.where(mask == 0)] == torch.zeros(3).to(device)
+                )
+                background_mask[torch.where(mask == 0)] = background
+                image = torch.clamp(image + background_mask, 0.0, 1.0)
+            images.append(image)
+
+        images = torch.cat(images, dim=0).permute(0, 3, 1, 2)
+        masks = torch.cat(masks, dim=0)
+        # rgb_mask = torch.cat(rgb_mask, dim=0)
+
+        if show:
+            with torch.no_grad():
+                fig, axs = plt.subplots(
+                    1 + (num_views - 1) // 4, min(4, num_views), figsize=(89.6, 22.4)
+                )
+                for i in range(num_views):
+                    if num_views == 1:
+                        ax = axs
+                    elif num_views <= 4:
+                        ax = axs[i]
+                    else:
+                        ax = axs[i // 4, i % 4]
+                    ax.imshow(images[i].permute(1, 2, 0).cpu().numpy())
+                plt.show()
+
+        if return_views == True:
+            return images, elev, azim
+        else:
+            return images
+
 
 if __name__ == "__main__":
     mesh = Mesh("sample.obj")
