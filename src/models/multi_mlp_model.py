d09b805b90d13b595143e20770e52098d5e6a567
Maximilian Winter <winter.maximilian@protonmail.com>
2022-12-27 15:25:59 +0100

    Refactor multi MLP model. (multi-mlps)

--- src/models/multi_mlp_model.py
+++ src/models/multi_mlp_model.py
@@ -9,7 +9,6 @@ from src.utils.utils import device, gaussian3D
 
 
 class Text2MeshMultiMLP(Text2MeshExtended):
-
     def __init__(self, args, base_mesh):
         super().__init__(args, base_mesh)
 
@@ -20,76 +19,21 @@ class Text2MeshMultiMLP(Text2MeshExtended):
             mlp.reset_weights()
 
             mlp_dict[prompt] = mlp
-        
+
         self.mlp = nn.ModuleDict(mlp_dict)
 
         self.mask_backward = MaskBackward.apply
 
     def forward(self, vertices):
         # Prop. through MLPs
-        pred_rgb = None
-        pred_normal = None
-        for prompt, mlp in self.mlp.items():
-            pred_rgb_per_prompt, pred_normal_per_prompt = mlp(vertices)
-            inv_mask = 1 - self.masks[prompt]
-            if self.args.gaussian_blending:
-                weight = self.gaussian_weights[prompt]
-            else:
-                weight = inv_mask
-            pred_rgb_masked = pred_rgb_per_prompt*weight
-            pred_normal_masked = pred_normal_per_prompt*weight
-
-            if pred_rgb is not None:
-                pred_rgb += pred_rgb_masked
-            else:
-                pred_rgb = pred_rgb_masked
-
-            if pred_normal is not None:
-                pred_normal += pred_normal_masked
-            else:
-                pred_normal = pred_normal_masked
-
-
-        if self.args.round_renderer_gradients:
-            pred_rgb = self.num_backward(pred_rgb)
-            pred_normal = self.num_backward(pred_normal)
-
-        if self.initial_pred_rgb is None:
-            self.initial_pred_rgb = pred_rgb.clone().detach()
+        pred_rgb, pred_normal = self.prop_through_mlps(vertices)
 
         # Rendering, Augmentations and CLIP encoding per prompt
-        encoded_renders_dict_per_prompt = {}
-        rendered_images_per_prompt = None
-        for prompt in self.args.prompts:
-            inv_mask = 1 - self.masks[prompt]
-            if self.args.gaussian_blending:
-                weight = self.gaussian_weights[prompt]
-            else:
-                weight = inv_mask
-            # Get stylized mesh
-            if self.args.do_backward_masking:
-                pred_rgb_masked = self.mask_backward(pred_rgb, weight)
-                pred_normal_masked = self.mask_backward(pred_normal, weight)
-                
-                self.stylize_mesh(pred_rgb_masked, pred_normal_masked)
-            else:
-                self.stylize_mesh(pred_rgb, pred_normal)
-
-            center_point = torch.mean(vertices[inv_mask[:, 0].bool()], dim=0) # we use the part's COM
-            distance = torch.sum((vertices[inv_mask[:, 0].bool()] - center_point)**2, dim=1).sqrt().max()*2 # we use 2-times the part's expansion
-            (U, S, V) = torch.pca_lowrank(vertices[inv_mask[:, 0].bool()])
-            m = vertices[inv_mask[:, 0].bool()].shape[0]
-            encoded_renders_dict, rendered_images = self.render_and_encode(center_point, distance, S, V, m)
-
-            encoded_renders_dict_per_prompt[prompt] = encoded_renders_dict
-            if rendered_images_per_prompt is None:
-                rendered_images_per_prompt = rendered_images
-            else:
-                rendered_images_per_prompt = torch.cat([rendered_images_per_prompt, rendered_images], dim=0)
-
-        color_reg = self.get_color_reg_terms(pred_rgb)
-
-        self.previous_pred_rgb = pred_rgb.clone().detach()
+        (
+            encoded_renders_dict_per_prompt,
+            rendered_images_per_prompt,
+            color_reg,
+        ) = self.render_augment_encode(vertices, pred_rgb, pred_normal)
 
         return {
             "encoded_renders": encoded_renders_dict_per_prompt,
@@ -97,10 +41,15 @@ class Text2MeshMultiMLP(Text2MeshExtended):
             "color_reg": color_reg,
         }
 
-    def render_and_encode(self, center_point, distance, covariances, principal_axes, num_vertices):
+    def render_and_encode(
+        self, center_point, distance, covariances, principal_axes, num_vertices
+    ):
         # Rendering
         rendered_images, _, _ = self.renderer.render_sampled_views_along_principal_axes(
-            self.base_mesh,covariances, principal_axes,num_vertices,
+            self.base_mesh,
+            covariances,
+            principal_axes,
+            num_vertices,
             num_views=self.args.n_views,
             show=self.args.show,
             center_azim=self.args.frontview_center[0],
@@ -118,7 +67,10 @@ class Text2MeshMultiMLP(Text2MeshExtended):
             )
 
             geo_renders, _, _ = self.renderer.render_sampled_views_along_principal_axes(
-                self.base_mesh,covariances, principal_axes,num_vertices,
+                self.base_mesh,
+                covariances,
+                principal_axes,
+                num_vertices,
                 num_views=self.args.n_views,
                 show=self.args.show,
                 center_azim=self.args.frontview_center[0],
@@ -136,4 +88,84 @@ class Text2MeshMultiMLP(Text2MeshExtended):
         )
 
         return encoded_renders_dict, rendered_images
-        
+
+    def prop_through_mlps(self, vertices):
+        # Prop. through MLPs
+        pred_rgb = None
+        pred_normal = None
+        for prompt, mlp in self.mlp.items():
+            pred_rgb_per_prompt, pred_normal_per_prompt = mlp(vertices)
+            inv_mask = 1 - self.masks[prompt]
+            if self.args.gaussian_blending:
+                weight = self.gaussian_weights[prompt]
+            else:
+                weight = inv_mask
+            pred_rgb_masked = pred_rgb_per_prompt * weight
+            pred_normal_masked = pred_normal_per_prompt * weight
+
+            if pred_rgb is not None:
+                pred_rgb += pred_rgb_masked
+            else:
+                pred_rgb = pred_rgb_masked
+
+            if pred_normal is not None:
+                pred_normal += pred_normal_masked
+            else:
+                pred_normal = pred_normal_masked
+
+        if self.args.round_renderer_gradients:
+            pred_rgb = self.num_backward(pred_rgb)
+            pred_normal = self.num_backward(pred_normal)
+
+        if self.initial_pred_rgb is None:
+            self.initial_pred_rgb = pred_rgb.clone().detach()
+
+        return pred_rgb, pred_normal
+
+    def render_augment_encode(self, vertices, pred_rgb, pred_normal):
+        # Rendering, Augmentations and CLIP encoding per prompt
+        encoded_renders_dict_per_prompt = {}
+        rendered_images_per_prompt = None
+        for prompt in self.args.prompts:
+            inv_mask = 1 - self.masks[prompt]
+            if self.args.gaussian_blending:
+                weight = self.gaussian_weights[prompt]
+            else:
+                weight = inv_mask
+            # Get stylized mesh
+            if self.args.do_backward_masking:
+                pred_rgb_masked = self.mask_backward(pred_rgb, weight)
+                pred_normal_masked = self.mask_backward(pred_normal, weight)
+
+                self.stylize_mesh(pred_rgb_masked, pred_normal_masked)
+            else:
+                self.stylize_mesh(pred_rgb, pred_normal)
+
+            center_point = torch.mean(
+                vertices[inv_mask[:, 0].bool()], dim=0
+            )  # we use the part's COM
+            distance = (
+                torch.sum((vertices[inv_mask[:, 0].bool()] - center_point) ** 2, dim=1)
+                .sqrt()
+                .max()
+                * 2
+            )  # we use 2-times the part's expansion
+            (U, S, V) = torch.pca_lowrank(vertices[inv_mask[:, 0].bool()])
+            m = vertices[inv_mask[:, 0].bool()].shape[0]
+            encoded_renders_dict, rendered_images = self.render_and_encode(
+                center_point, distance, S, V, m
+            )
+
+            encoded_renders_dict_per_prompt[prompt] = encoded_renders_dict
+            if rendered_images_per_prompt is None:
+                rendered_images_per_prompt = rendered_images
+            else:
+                rendered_images_per_prompt = torch.cat(
+                    [rendered_images_per_prompt, rendered_images], dim=0
+                )
+
+        color_reg = self.get_color_reg_terms(pred_rgb)
+
+        self.previous_pred_rgb = pred_rgb.clone().detach()
+
+        return encoded_renders_dict_per_prompt, rendered_images_per_prompt, color_reg
