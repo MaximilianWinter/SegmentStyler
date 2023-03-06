befd0629bfc59400ccc44957fcae9572b2b75f5c
Maximilian Winter <winter.maximilian@protonmail.com>
2023-03-06 13:30:48 +0100

    Cleanup. Add one sample. (main)

---
+++ ablation_study_b_to_g.sh
@@ -0,0 +1,206 @@
+for version in {0..250}
+do
+    python main.py \
+    --sigma 5.0 \
+    --clamp tanh \
+    --n_normaugs 4 \
+    --n_augs 1 \
+    --normmincrop 0.1 \
+    --normmaxcrop 0.1 \
+    --frontview \
+    --frontview_std 4 \
+    --clipavg view \
+    --lr_decay 0.9 \
+    --clamp tanh \
+    --normclamp tanh \
+    --maxcrop 1.0 \
+    --save_render \
+    --seed 78942387 \
+    --save_render \
+    --frontview_center 5.4 -0.5 \
+    --colordepth 2 \
+    --normdepth 2 \
+    --n_iter 1500 \
+    --learning_rate 0.005 \
+    --output_dir evaluation_b \
+    --experiment_group evaluation_b \
+    --width 32 \
+    --depth 2 \
+    --model_name Text2MeshBackwardMasking \
+    --loss_name multi_mlp_loss \
+    --do_backward_masking \
+    --dataset PartGlotData \
+    --eval_version $version
+
+    python main.py \
+    --sigma 5.0 \
+    --clamp tanh \
+    --n_normaugs 4 \
+    --n_augs 1 \
+    --normmincrop 0.1 \
+    --normmaxcrop 0.1 \
+    --frontview \
+    --frontview_std 4 \
+    --clipavg view \
+    --lr_decay 0.9 \
+    --clamp tanh \
+    --normclamp tanh \
+    --maxcrop 1.0 \
+    --save_render \
+    --seed 78942387 \
+    --save_render \
+    --frontview_center 5.4 -0.5 \
+    --colordepth 2 \
+    --normdepth 2 \
+    --n_iter 1500 \
+    --learning_rate 0.005 \
+    --output_dir evaluation_c \
+    --experiment_group evaluation_c \
+    --width 32 \
+    --depth 2 \
+    --model_name Text2MeshMultiMLP \
+    --loss_name multi_mlp_loss \
+    --do_backward_masking \
+    --dataset PartGlotData \
+    --eval_version $version
+
+    python main.py \
+    --sigma 5.0 \
+    --clamp tanh \
+    --n_normaugs 4 \
+    --n_augs 1 \
+    --normmincrop 0.1 \
+    --normmaxcrop 0.1 \
+    --frontview \
+    --frontview_std 4 \
+    --clipavg view \
+    --lr_decay 0.9 \
+    --clamp tanh \
+    --normclamp tanh \
+    --maxcrop 1.0 \
+    --save_render \
+    --seed 78942387 \
+    --save_render \
+    --frontview_center 5.4 -0.5 \
+    --colordepth 2 \
+    --normdepth 2 \
+    --n_iter 1500 \
+    --learning_rate 0.005 \
+    --output_dir evaluation_d \
+    --experiment_group evaluation_d \
+    --width 32 \
+    --depth 2 \
+    --model_name Text2MeshMultiMLP \
+    --loss_name multi_mlp_loss \
+    --do_backward_masking \
+    --biased_views \
+    --dataset PartGlotData \
+    --sample $sample \
+    --eval_version $version
+
+    python main.py \
+    --sigma 5.0 \
+    --clamp tanh \
+    --n_normaugs 4 \
+    --n_augs 1 \
+    --normmincrop 0.1 \
+    --normmaxcrop 0.1 \
+    --frontview \
+    --frontview_std 4 \
+    --clipavg view \
+    --lr_decay 0.9 \
+    --clamp tanh \
+    --normclamp tanh \
+    --maxcrop 1.0 \
+    --save_render \
+    --seed 78942387 \
+    --save_render \
+    --frontview_center 5.4 -0.5 \
+    --colordepth 2 \
+    --normdepth 2 \
+    --n_iter 1500 \
+    --learning_rate 0.005 \
+    --output_dir evaluation_e \
+    --experiment_group evaluation_e \
+    --width 32 \
+    --depth 2 \
+    --model_name Text2MeshMultiMLP \
+    --loss_name multi_mlp_loss \
+    --do_backward_masking \
+    --biased_views \
+    --gaussian_blending \
+    --final_gaussian_blending \
+    --dataset PartGlotData \
+    --eval_version $version
+
+    python main.py \
+    --sigma 5.0 \
+    --clamp tanh \
+    --n_normaugs 4 \
+    --n_augs 1 \
+    --normmincrop 0.1 \
+    --normmaxcrop 0.1 \
+    --frontview \
+    --frontview_std 4 \
+    --clipavg view \
+    --lr_decay 0.9 \
+    --clamp tanh \
+    --normclamp tanh \
+    --maxcrop 1.0 \
+    --save_render \
+    --seed 78942387 \
+    --save_render \
+    --frontview_center 5.4 -0.5 \
+    --colordepth 2 \
+    --normdepth 2 \
+    --n_iter 1500 \
+    --learning_rate 0.005 \
+    --output_dir evaluation_f \
+    --experiment_group evaluation_f \
+    --width 32 \
+    --depth 2 \
+    --model_name Text2MeshLabelPropagation \
+    --loss_name label_propagation_loss \
+    --do_backward_masking \
+    --biased_views \
+    --gaussian_blending \
+    --final_gaussian_blending \
+    --dataset PartGlotData \
+    --eval_version $version \
+    --weights_path "logs/evaluation_dir/evaluation_d/version_$version/final_mlp.pt" \
+    --optimize_learned_labels
+
+    python main.py \
+    --sigma 5.0 \
+    --clamp tanh \
+    --n_normaugs 4 \
+    --n_augs 1 \
+    --normmincrop 0.1 \
+    --normmaxcrop 0.1 \
+    --frontview \
+    --frontview_std 4 \
+    --clipavg view \
+    --lr_decay 0.9 \
+    --clamp tanh \
+    --normclamp tanh \
+    --maxcrop 1.0 \
+    --save_render \
+    --seed 78942387 \
+    --save_render \
+    --frontview_center 5.4 -0.5 \
+    --colordepth 2 \
+    --normdepth 2 \
+    --n_iter 1500 \
+    --learning_rate 0.005 \
+    --output_dir evaluation_g \
+    --experiment_group evaluation_g \
+    --width 32 \
+    --depth 2 \
+    --model_name Text2MeshMultiMLP \
+    --loss_name multi_mlp_loss \
+    --do_backward_masking \
+    --biased_views \
+    --use_gt_masks \
+    --dataset PartGlotData \
+    --eval_version $version
+done
---
+++ example_run.sh
@@ -0,0 +1,36 @@
+python main.py \
+--sigma 5.0 \
+--clamp tanh \
+--n_normaugs 4 \
+--n_augs 1 \
+--normmincrop 0.1 \
+--normmaxcrop 0.1 \
+--frontview \
+--frontview_std 4 \
+--clipavg view \
+--lr_decay 0.9 \
+--clamp tanh \
+--normclamp tanh \
+--maxcrop 1.0 \
+--save_render \
+--seed 78942387 \
+--save_render \
+--frontview_center 5.4 -0.5 \
+--colordepth 2 \
+--normdepth 2 \
+--n_iter 1500 \
+--learning_rate 0.005 \
+--output_dir evaluation_no_blend_new \
+--experiment_group evaluation_no_blend_new \
+--width 32 \
+--depth 2 \
+--model_name Text2MeshMultiMLP \
+--loss_name multi_mlp_loss \
+--do_backward_masking \
+--biased_views \
+--dataset PartGlotData \
+--sample 601 \
+--prompt "snowflake pattern arm" \
+--prompt "metallic leg" \
+--prompt "honeycomb back" \
+--prompt "wooden seat" 
